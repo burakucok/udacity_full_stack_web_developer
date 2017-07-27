d6452e40bfc38fc4185122069dd74bcedc8f02d7
Burak Üçok <ucokburak@gmail.com>
2017-07-27 13:23:28 +0300

    Update entertainment_center.py
    comment added

--- Project 1 - Movie Trailer Website/entertainment_center.py
+++ Project 1 - Movie Trailer Website/entertainment_center.py
@@ -2,10 +2,11 @@ import fresh_tomatoes
 import media
 
 movies = []
+#add movies
 movies.append(media.Movie("The Shawshank Redemption",
                           "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SY1000_CR0,0,672,1000_AL_.jpg",
                           "https://www.youtube.com/watch?v=6hB3S9bIaco"))
-                          
+
 movies.append(media.Movie("The Godfather",
                           "https://images-na.ssl-images-amazon.com/images/M/MV5BZTRmNjQ1ZDYtNDgzMy00OGE0LWE4N2YtNTkzNWQ5ZDhlNGJmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,704,1000_AL_.jpg",
                           "https://www.youtube.com/watch?v=sY1S34973zA"))
@@ -21,5 +22,5 @@ movies.append(media.Movie("The Dark Knight",
 movies.append(media.Movie("12 Angry Men",
                           "https://images-na.ssl-images-amazon.com/images/M/MV5BODQwOTc5MDM2N15BMl5BanBnXkFtZTcwODQxNTEzNA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
                           "https://www.youtube.com/watch?v=A7CBKT0PWFA"))
-
+#open web page
 fresh_tomatoes.open_movies_page(movies)
