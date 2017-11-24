2b0fee90efbfae2b62bb58028765ab35625f703d
Burak Üçok <ucokburak@gmail.com>
2017-11-24 22:14:41 +0300

    Update application.js

--- Project 5 - Neighborhood Map/js/application.js
+++ Project 5 - Neighborhood Map/js/application.js
@@ -306,9 +306,26 @@ function initMap() {
 								photoUrl + '"></div></a>';								
 			} 
 		  },
-		  error: function(exception) {
+		  error: function(jqXHR, exception) {
+			var msg = '';
+			if (jqXHR.status === 0) {
+			    msg = 'Not connect. Verify Network.';
+			} else if (jqXHR.status == 404) {
+			    msg = 'Requested page not found. [404]';
+			} else if (jqXHR.status == 500) {
+			    msg = 'Internal Server Error [500].';
+			} else if (exception === 'parsererror') {
+			    msg = 'Requested JSON parse failed.';
+			} else if (exception === 'timeout') {
+			    msg = 'Time out error.';
+			} else if (exception === 'abort') {
+			    msg = 'Ajax request aborted.';
+			} else {
+			    msg = 'Uncaught Error.' + jqXHR.responseText;
+			}	  
+			  
 			marker.content = "<h3>" + marker.title + "</h3><br>" +
-					 "<p>Oppss, Could not load data from Foursquare!!</p>";                             
+					 "<p>Oppss,Could not load data from Foursquare!!<br>" + msg + "</p>";                             
 		  }  	
 		});			
 		
